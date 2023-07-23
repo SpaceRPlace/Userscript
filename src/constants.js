1eb966a48680606775d353e23dc214106403d06e
Emlyn <71500044+emlynlj@users.noreply.github.com>
2023-07-23 02:08:43 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://place.emlynlj.co.uk:3000/ws';
+export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'ws://place.emlynlj.co.uk:3000/ws';
 export const USERSCRIPT_REVISION = ___GIT_HASH___ || 'development';
